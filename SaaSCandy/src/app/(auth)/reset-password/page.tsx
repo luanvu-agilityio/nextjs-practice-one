efd627fbe2f6a169a854004039eec94948b49104
Luan Vu <luan.vu@asnet.com.vn>
2025-11-03 02:57:46 +0700

    feat: test deploy

--- SaaSCandy/src/app/(auth)/reset-password/page.tsx
+++ SaaSCandy/src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,18 @@
+'use client';
 import ResetPasswordPageContent from '@/components/pages/ResetPasswordPageContent';
+import { Suspense } from 'react';
 
 const ResetPasswordPage = () => {
   return (
-    <main className='min-h-screen flex items-center justify-center py-12'>
+    <Suspense
+      fallback={
+        <div className='min-h-screen flex items-center justify-center bg-light-gray'>
+          <div className='w-16 h-16 border-4 border-primary border-t-transparent rounded-full animate-spin' />
+        </div>
+      }
+    >
       <ResetPasswordPageContent />
-    </main>
+    </Suspense>
   );
 };
 export default ResetPasswordPage;
